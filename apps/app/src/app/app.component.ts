d6c1102422afbcfdb9cd44c08630978b888415c0
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-11-23 15:07:13 +0900

    ガチャを回してレストラン名を1つ表示できる

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Restaurant, RESTAURANT_LIST } from '@lunch-gacha/data';
 
 @Component({
   selector: 'lunch-gacha-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly restaurantList: Restaurant[] = RESTAURANT_LIST;
+
+  gachaResult: Restaurant | null = null;
+
+  turnGacha(): void {
+    const randomNum = Math.floor(Math.random() * this.restaurantList.length);
+    this.gachaResult = this.restaurantList[randomNum];
+  }
+}
