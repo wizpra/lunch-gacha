7b9d8a8a7a6e8a561ecc440ca802dda9543f307e
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-12-12 09:08:56 +0900

    header の実装

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
