e7b59d24ffc3114b28af20ea84b8f76bfd36989d
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-12-12 23:57:23 +0900

    Restaurant 型にジャンルを追加

--- libs/data/src/lib/data.ts
+++ libs/data/src/lib/data.ts
@@ -4,165 +4,236 @@ export const RESTAURANT_LIST: Restaurant[] = [
   {
     name: '赤坂 四川飯店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13000434/',
+    genre: [],
   },
   {
     name: 'エリックサウス 東京ガーデンテラス店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13195343/',
+    genre: [],
   },
   {
     name: 'YONA YONA BEER WORKS 赤坂店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13160944/',
+    genre: [],
   },
   {
     name: '陳麻婆豆腐 東急プラザ赤坂店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13002291/',
+    genre: [],
   },
   {
     name: '唐朝刀削麺 赤坂見附店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13126882/',
+    genre: [],
   },
   {
     name: 'Anchor Point',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13197201/',
+    genre: [],
   },
   {
     name: '鉄板ラッシュ! エチカフィット永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13159437/',
+    genre: [],
   },
-  { name: 'こいけ', url: 'https://tabelog.com/tokyo/A1308/A130803/13021963/' },
+  {
+    name: 'こいけ',
+    url: 'https://tabelog.com/tokyo/A1308/A130803/13021963/',
+    genre: [],
+  },
   {
     name: 'GARB CENTRAL',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13193501/',
+    genre: [],
   },
   {
     name: '炭焼きステーキくに 赤坂店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13024864/',
+    genre: [],
   },
   {
     name: 'アジアンビストロ Dai 東京ガーデンテラス店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13193873/',
+    genre: [],
   },
   {
     name: '大地の恵み 北海道 永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13160314/',
+    genre: [],
   },
   {
     name: '上海大飯店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13019535/',
+    genre: [],
   },
   {
     name: '日の陣 エチカフィット永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13158712/',
+    genre: [],
   },
   {
     name: 'いち膳 エチカフィット永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13159028/',
+    genre: [],
   },
   {
     name: '和食処 梅林',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13105013/',
+    genre: [],
   },
-  { name: '蓼科', url: 'https://tabelog.com/tokyo/A1308/A130801/13175336/' },
-  { name: '慶賓樓', url: 'https://tabelog.com/tokyo/A1308/A130801/13020959/' },
-  { name: '万ん卯', url: 'https://tabelog.com/tokyo/A1308/A130801/13199079/' },
+  {
+    name: '蓼科',
+    url: 'https://tabelog.com/tokyo/A1308/A130801/13175336/',
+    genre: [],
+  },
+  {
+    name: '慶賓樓',
+    url: 'https://tabelog.com/tokyo/A1308/A130801/13020959/',
+    genre: [],
+  },
+  {
+    name: '万ん卯',
+    url: 'https://tabelog.com/tokyo/A1308/A130801/13199079/',
+    genre: [],
+  },
   {
     name: 'ティークロッシング  エチカフィット永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13159596/',
+    genre: [],
   },
   {
     name: '手むすびむすび エチカフィット永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13159029/',
+    genre: [],
   },
   {
     name: 'ペルラン',
     url: 'https://tabelog.com/tokyo/A1308/A130802/13032083/',
+    genre: [],
   },
   {
     name: '野田焼売店 紀尾井本店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13241544/',
+    genre: [],
   },
   {
     name: 'レストラン アイリス',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13034934/',
+    genre: [],
   },
-  { name: '南甫園', url: 'https://tabelog.com/tokyo/A1308/A130803/13049902/' },
+  {
+    name: '南甫園',
+    url: 'https://tabelog.com/tokyo/A1308/A130803/13049902/',
+    genre: [],
+  },
   {
     name: '赤坂 鳥幸',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13196024/',
+    genre: [],
   },
   {
     name: '手打ちそば そばしき 紀尾井町店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13257185/',
+    genre: [],
   },
   {
     name: 'カレーショップ C&C  Echika fit 永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13158711/',
+    genre: [],
   },
-  { name: '米幸', url: 'https://tabelog.com/tokyo/A1308/A130801/13250882/' },
+  {
+    name: '米幸',
+    url: 'https://tabelog.com/tokyo/A1308/A130801/13250882/',
+    genre: [],
+  },
   {
     name: '八天堂 東京メトロ永田町駅店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13159510/',
+    genre: [],
   },
   {
     name: 'みどり食堂',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13130940/',
+    genre: [],
   },
   {
     name: '喫茶カルム',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13109105/',
+    genre: [],
   },
   {
     name: 'ビーエスビー コーヒー ロースターズ',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13199505/',
+    genre: [],
   },
   {
     name: '珈琲果実 JA共済ビル店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13131164/',
+    genre: [],
   },
   {
     name: '串の坊 赤坂東急プラザ店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13013440/',
+    genre: [],
   },
-  { name: 'BASE17', url: 'https://tabelog.com/tokyo/A1308/A130801/13213286/' },
+  {
+    name: 'BASE17',
+    url: 'https://tabelog.com/tokyo/A1308/A130801/13213286/',
+    genre: [],
+  },
   {
     name: 'エクセルシオール カフェ 永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13044117/',
+    genre: [],
   },
   {
     name: 'サンマルクカフェ ビックカメラ赤坂見附駅店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13158913/',
+    genre: [],
   },
   {
     name: 'タリーズコーヒー 赤坂東急プラザ店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13103685/',
+    genre: [],
   },
   {
     name: 'ティーラウンジ 佳風',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13255029/',
+    genre: [],
   },
   {
     name: 'スターバックスコーヒー 東京ガーデンテラス紀尾井町店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13195377/',
+    genre: [],
   },
-  { name: '喫茶室', url: 'https://tabelog.com/tokyo/A1308/A130803/13107692/' },
+  {
+    name: '喫茶室',
+    url: 'https://tabelog.com/tokyo/A1308/A130803/13107692/',
+    genre: [],
+  },
   {
     name: '永田町オーシャン',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13211917/',
+    genre: [],
   },
   {
     name: 'フレッシュネスバーガー 東京ガーデンテラス紀尾井町店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13227644/',
+    genre: [],
   },
   {
     name: '手作り弁当 エフエル',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13188558/',
+    genre: [],
   },
   {
     name: 'ソジュマニア 赤坂店',
     url: 'https://tabelog.com/tokyo/A1308/A130801/13264822/',
+    genre: [],
   },
   {
     name: 'Re.Ra.Ku PRO ランニング＆カフェ 永田町店',
     url: 'https://tabelog.com/tokyo/A1308/A130803/13264946/',
+    genre: [],
   },
 ];
