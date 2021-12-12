ffa1eade2a122e39c8fb2fb8e063bc8cb6337578
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-12-12 23:43:52 +0900

    ogp のサムネイルを取得して表示 + 通信中にspinner 表示

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Restaurant, RESTAURANT_LIST } from '@lunch-gacha/data';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'lunch-gacha-root',
@@ -7,12 +9,40 @@ import { Restaurant, RESTAURANT_LIST } from '@lunch-gacha/data';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
+
   readonly restaurantList: Restaurant[] = RESTAURANT_LIST;
 
   gachaResult: Restaurant | null = null;
+  imageUrl: string | null = null;
+
+  isNotGachaYet = true;
+  isLoading = false;
+
+  imageRegex = new RegExp(
+    /https:\/\/tblg.k-img.com\/restaurant\/images\/.*?\.jpg/
+  );
 
   turnGacha(): void {
+    if (this.isNotGachaYet) this.isNotGachaYet = false;
     const randomNum = Math.floor(Math.random() * this.restaurantList.length);
     this.gachaResult = this.restaurantList[randomNum];
+
+    this.isLoading = true;
+    this.http
+      .get(`${environment.proxy}${this.gachaResult.url}`, {
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (res) => {
+          const ogImageElement = this.imageRegex.exec(res);
+          if (ogImageElement == null) return;
+          this.imageUrl = ogImageElement[0] ?? null;
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => (this.isLoading = false),
+      });
   }
 }
